98f6fc2f996f8a42da767dc21557ee4abc5d3b57
Donavon Clay <donavon.clay21@gmail.com>
2022-12-02 05:09:25 +0000

    added split_sprite and parabolic

---
+++ src/parabolic.sv
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+`default_nettype none
+
+module parabolic (
+    input wire clk_in, rst_in, split, veggie_gone_in,
+    output logic[2:0] x_speed_out, y_speed_out,
+    output logic up
+);
+
+
+endmodule
+
+`default_nettype wire
